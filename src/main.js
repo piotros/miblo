4794b55d4cf9f823fe941759f19c056625cf3194
piotros <piotros@wp.pl>
2016-05-12 18:42:24 +0200

    Add i18n plugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue            from 'vue'
 import VueRouter      from 'vue-router'
 import VueResource    from 'vue-resource'
+import VueI18n        from 'vue-i18n'
 import { sync }       from 'vuex-router-sync'
 import App            from 'components/app.vue'
 import configRouter   from 'router-config.js'
@@ -8,6 +9,7 @@ import store          from 'vuex/store'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
+Vue.use(VueI18n)
 
 const router = new VueRouter({
     hashbang: false
